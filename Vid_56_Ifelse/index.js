125d028390b03067ee55556ae9321f95c7167017
Parth Patel <creatinganewway@gmail.com>
2025-02-22 12:50:35 +0530

    if else in js

---
+++ Vid_56_Ifelse/index.js
@@ -0,0 +1,15 @@
+console.log("Hell world")
+
+let age = 4;
+let grace = 2
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)  //Exponent operator
+
+if(age>18){
+    console.log("You can Vote")
+}else{
+    console.log("No right to vote")
+}
