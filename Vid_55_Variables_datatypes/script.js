84efbe6d095794100b2f305f9b1f961c205b19c2
Parth Patel <creatinganewway@gmail.com>
2025-02-22 12:49:57 +0530

    datatypes in JS

---
+++ Vid_55_Variables_datatypes/script.js
@@ -0,0 +1,36 @@
+console.log("Hey this is the tutorial number 55")
+
+let a = 4;
+var b = 3;
+var c  = 'C';
+var d = "PATEL";
+// console.log(a + b + 8)
+// console.log(typeof a , typeof b , typeof c, typeof d)
+
+
+// {
+//     let a = 9;
+//     console.log(a)
+// }
+// console.log(a)
+
+let x = "Parth Bhai";
+let y = 21;
+let z = 2.56;
+const p = true;
+let s = undefined;
+let r = null;
+
+console.log(x,y,z,p,s,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof s,typeof r);
+
+
+let o = {
+    "Name": "PArth",
+    "Job ROLE": "SDE"
+}
+
+console.log(o)
+
+o.Salary = "100cr";
+console.log(o)
