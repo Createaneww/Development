c8039002e9d32551c7307a040c0075cf47b4bc23
Parth Patel <creatinganewway@gmail.com>
2025-02-23 13:47:29 +0530

    loops in js

---
+++ Vid_57_Loops/file.js
@@ -0,0 +1,37 @@
+let a = 1;
+// for(let i = 0 ; i<100 ; i++){
+//     console.log(a+i)
+// }
+
+let obj= {
+    name: "Parth",
+    role: "SDE",
+    company_name: "Manvar"
+}
+
+// forin loop
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key , element)
+// }
+
+//forof loop
+// for (const element of "Parth") {
+//     console.log(element)
+// }
+
+
+
+// while loop
+// let i = 1;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+
+let i = 1;
+do{
+    console.log(i)
+    i++
+}while(i<6);
