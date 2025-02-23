5e2d092afc4fa325867bff5aec621db7f8e7ef58
Parth Patel <creatinganewway@gmail.com>
2025-02-23 13:47:45 +0530

    func in js

---
+++ Vid_58_Functions/file.js
@@ -0,0 +1,22 @@
+// function nice(name){
+//     console.log("Hey " + name + " your are cool!")
+//     console.log("Hey " + name + " your are awesome!")
+//     console.log("Hey " + name + " your are smart!")
+//     console.log("Hey " + name + " your are best!")
+//     console.log("Hey " + name + " your are confident!")
+// }
+function sum(a,b){
+    // console.log(a + b)
+    return a+b;
+}
+// result = sum(2,6);
+// console.log("The sum of two nnumber is" , result)
+// nice("Parth");
+
+// arrow function
+const func1 = (x)=>{
+    console.log("I am a arrow function" , x)
+}
+func1(23);
+func1(32);
+func1(22);
