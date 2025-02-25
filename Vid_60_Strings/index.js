f505932f2a359c63f6ccfd45d5e2f597bd8223fa
Parth Patel <creatinganewway@gmail.com>
2025-02-25 17:31:17 +0530

    Strings

---
+++ Vid_60_Strings/index.js
@@ -0,0 +1,18 @@
+console.log("This is Strings tutotrail")
+
+let a = "Manvar";
+
+console.log(a[5])
+console.log(a.length)
+
+
+
+let my_name = "Parth";
+let friend_name = "Jolly";
+console.log("My name is " + my_name + " and my friend name is " + friend_name)
+console.log(`My name is ${my_name} and my friend name is ${friend_name}`)  //backtic use-case
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+
+console.log(a.slice(1,4))  // last wala digit include nhi log i.e here 4
+console.log(a.replace("Man" , "Fan")) // agar same same do bar hua to phele wala replace hoga
