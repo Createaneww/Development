c458f6af10c7e19c6e93d7adb35fb0d38a74b01e
Parth Patel <creatinganewway@gmail.com>
2025-03-02 13:06:31 +0530

    use of DOM

---
+++ Vid_67_DOM_USE/script.js
@@ -0,0 +1,3 @@
+document.body.firstElementChild
+document.body.firstElementChild.childNodes  // text and comment nodes will be included
+document.body.firstElementChild.children // only html elements will be included
