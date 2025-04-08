ac6abb4dc67bd621dcb82acdc2b4a9eaa0740e08
Parth Patel <creatinganewway@gmail.com>
2025-04-08 10:12:39 +0530

    Dummy Data Generator
    with the help of js and mongoDb we have created random , on an express app

--- VId_97_Dummy_data_generator_on_express/main.js
+++ VId_97_Dummy_data_generator_on_express/main.js
@@ -16,12 +16,38 @@ import mongoose from "mongoose";
 import express from "express";
 import { schema } from "./models/schema.js";
 
-let conn = await mongoose.connect("mongodb://localhost:27017/company")
+let conn = mongoose.connect("mongodb://localhost:27017/company")
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+
+const GetRandom = (arr) => {
+  let rno = Math.floor(Math.random() * (arr.length - 1))
+  return arr[rno]
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { foo: 'FOO' })
+})
+app.get('/generate', async (req, res) => {
+  let randonName = ["Mohan", "Sita", "Priya", "Vanshi"]
+  let randomLang = ["CPP", "Java", "CSS", "Python"]
+  let randomCity = ["Dholakpur", "Bheempur", "Najabghad", "Ghaziabad"]
+  //generate random data
+  await schema.deleteMany({})
+  for (let i = 0; i < 10; i++) {
+    let e = await schema.create({
+      name: GetRandom(randonName),
+      salary: Math.floor(Math.random()*50000),
+      language: GetRandom(randomLang),
+      city: GetRandom(randomCity),
+      isManager: (Math.random()>0.5)?true:false
+    })
+     console.log(e);
+
+  }
+  res.render('index', { foo: 'FOO' })
 })
 
 app.listen(port, () => {
