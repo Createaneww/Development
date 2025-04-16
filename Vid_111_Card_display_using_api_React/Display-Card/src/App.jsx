6044a0347818396a5c0ddc5c58a5d4a93fd5cdd2
Parth Patel <creatinganewway@gmail.com>
2025-04-16 10:23:29 +0530

    card made

--- Vid_111_Card_display_using_api_React/Display-Card/src/App.jsx
+++ Vid_111_Card_display_using_api_React/Display-Card/src/App.jsx
@@ -11,11 +11,16 @@ function App() {
 
 
 
-  
+
 
   return (
     <>
-      <div className="container bg-red-700">hello world</div>
+      <div className="container bg-gray-500 mt-5 w-[25vw] flex flex-col ml-6 rounded-2xl">
+      <div className="title font-bold text-xl bg-gray-600 p-4"><h1 className=''>Title</h1></div>
+      <div className="userid p-2">ID</div>
+      <div className="userid p-2">USErid</div>
+      <div className="userid p-[100px] border border-black rounded-2xl">BODY</div>
+      </div>
     </>
   )
 }
