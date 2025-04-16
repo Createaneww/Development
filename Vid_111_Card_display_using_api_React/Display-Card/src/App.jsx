7ac4d69904b42dcd0b2294aa477ac1d999837db4
Parth Patel <creatinganewway@gmail.com>
2025-04-16 11:05:16 +0530

    learned using api and list renderring using map fucntion

--- Vid_111_Card_display_using_api_React/Display-Card/src/App.jsx
+++ Vid_111_Card_display_using_api_React/Display-Card/src/App.jsx
@@ -1,25 +1,37 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  const [ID, setID] = useState(0)
-  const [userId, setuserId] = useState(0)
-  const [title, settitle] = useState(0)
-  const [body, setbody] = useState(0)
+  const [cards, setcards] = useState([])
 
+  const fetchdata = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setcards(data)
+    console.log(data);
+    
+  }
+
+  useEffect(() => {
+    fetchdata()
+  }, [])
 
 
 
 
   return (
     <>
-      <div className="container bg-gray-500 mt-5 w-[25vw] flex flex-col ml-6 rounded-2xl">
-      <div className="title font-bold text-xl bg-gray-600 p-4"><h1 className=''>Title</h1></div>
-      <div className="userid p-2">ID</div>
-      <div className="userid p-2">USErid</div>
-      <div className="userid p-[100px] border border-black rounded-2xl">BODY</div>
+      <div className="container flex flex-wrap gap-[34px] rounded-2xl">
+        {cards.map((card)=>{
+         return <div className="cards gap-[15px] bg-gray-500 max-w-[23vw] max-h-[98vh]">
+        <div className="title border border-black font-bold text-xl bg-gray-600 p-4"><h1 className=''>{card.title}</h1></div>
+        <div className="userid border border-black p-2">ID: {card.id}</div>
+        <div className="userid border border-black p-2"> userID: {card.userId}</div>
+        <div className="userid p-[100px] border border-black">{card.body}</div>
+        </div> 
+        })}
       </div>
     </>
   )
